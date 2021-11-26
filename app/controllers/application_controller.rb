f937a6468858bb8255b5f5384c59b70a3c6e3219
Samantha Diaz <samidiaz3@gmail.com>
2021-11-26 17:05:37 -0500

    created and routed log in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < Sinatra::Base
   configure do
     #set :public_folder, 'public'
     enable :sessions
-    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }     #used: ruby -e "require 'securerandom'; puts SecureRandom.hex(64)"
+    set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) } #used: ruby -e "require 'securerandom'; puts SecureRandom.hex(64)"
     set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
+  end
+
+  get "/login" do 
+    erb :login
   end
 
 end
