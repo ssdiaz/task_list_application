b6174baf0610e8d7d54850a191a5389c4922cfdd
Samantha Diaz <samidiaz3@gmail.com>
2021-11-26 17:47:02 -0500

    added /account route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,29 +10,29 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    erb :'welcome'
   end
 
-  get "/login" do 
-    erb :login
+  get '/login' do 
+    erb :'login'
   end
 
-  post "/login" do
+  post '/login' do
     @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
       #redirect to "/account"
     end
-    # erb :error
+    # erb :'error'
   end
 
-  get "/account" do
+  get '/account' do
     @current_user = User.find_by_id(session[:user_id])
     if @current_user
-      erb :account
+      erb :'/user/account'
     else
-      erb :error
+      erb :'error'
     end
   end
 
