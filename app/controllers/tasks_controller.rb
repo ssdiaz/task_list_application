f0f1a641a64a37f8c9f5403663d6d1ada7cd0155
Samantha Diaz <samidiaz3@gmail.com>
2021-12-01 10:24:52 -0500

    deletes a task and only creates a task if task.name is not empty

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,16 @@ class TasksController < ApplicationController
     end
 
     post '/tasks/new' do
-        @task = Task.create(params[:task])
+        if !params["task"]["name"].empty?
+            @task = Task.create(params[:task])
+        end
+
+        redirect '/tasks'
+    end
+
+    delete '/tasks/:id' do
+        @task = Task.find_by_id(params[:id])
+        @task.delete
         redirect '/tasks'
     end
 
