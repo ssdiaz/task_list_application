2671798e9b95a1c9799614ca7321c9624aaef343
Samantha Diaz <samidiaz3@gmail.com>
2021-12-01 13:38:35 -0500

    most of tasks table working in tasks index view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,34 @@
 class TasksController < ApplicationController
     
-    get '/tasks' do 
+    get '/tasks' do  #show all tasks page -- need to make for account tho
         @tasks = Task.all
+        @task = Task.find_by_id(params[:id])
+        @items_completed = @tasks.count {|task| task.status == "Complete"}
         erb :'/tasks/index'
     end
 
-    post '/tasks/new' do
+    post '/tasks/new' do #making new task
         if !params["task"]["name"].empty?
-            @task = Task.create(params[:task])
+            @task = Task.new(params[:task])
+            @task.status = "Open"
+            @task.save
         end
-
         redirect '/tasks'
     end
 
-    delete '/tasks/:id' do
+    patch '/tasks/:id' do #edit patch status -- need to do when ticked
+        @task = Task.find_by_id(params[:id])
+        @task.status = "Complete"
+        @task.save
+        redirect '/tasks'
+    end
+
+
+    delete '/tasks/:id' do #delete task
         @task = Task.find_by_id(params[:id])
         @task.delete
         redirect '/tasks'
     end
-
-
 
 
 
