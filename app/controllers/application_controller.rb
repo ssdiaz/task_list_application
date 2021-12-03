f864f948d93bc06abda5a4b6419edf94dd5a2f0d
Samantha Diaz <samidiaz3@gmail.com>
2021-12-03 00:07:21 -0500

    more stlying css amendments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    @current_user = User.find_by_id(session[:user_id])
+    # @current_user = User.find_by_id(session[:user_id])
     erb :'welcome'
   end
 
